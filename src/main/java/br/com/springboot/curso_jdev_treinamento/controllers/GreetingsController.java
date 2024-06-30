b4cfb4f8de18bc3983697470f2a7437616b25b96
Diego Barcellos <diegocb81@gmail.com>
2024-06-30 02:32:23 -0300

    Criando o Endpoint de listar todos usuários

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,13 +1,16 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.springboot.curso_jdev_treinamento.model.Usuario;
 import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
 
@@ -41,5 +44,15 @@ public class GreetingsController {
     	usuarioRepository.save(usuario);/*Grava no banco de dados*/
     	
     	return "Olá Mundo " + nome;
+    }
+    
+    @GetMapping(value = "listatodos") /*Método de API*/
+    @ResponseBody /*Retorna os dados para o corpo da resposta*/
+    public ResponseEntity<List<Usuario>> listaUsuario() {
+    	
+    	List<Usuario> usuarios = usuarioRepository.findAll();/*Executa a consulta no banco de dados*/
+    	
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); /*Retorna a lista em JSON*/
+    	
     }
 }
